45a9af6deabbdbccfc7ef62c90e09bd31caf031e
leska-ver <lyaisan.333@yandex.ru>
2022-02-21 15:21:58 +0500

    forma-Jeka

--- mailingForma/sendmail.php
+++ mailingForma/sendmail.php
@@ -1,8 +1,9 @@
+<?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'phpmailer/src/Exception';
-require 'phpmailer/src/PHPMailer';
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer/php';
 
 $mail = new PHPMailer(true);
 $mail->Charset = 'UTF-8';
@@ -10,9 +11,9 @@ $mail->setLanguage('ru', 'phpmailer/language/');
 $mail->IsHTML(true);
 
 //От кого письмо
-$mail->setFrom('info@fls.guru', 'Фрилансер по жизни');
+$mail->setFrom('info@fls.guru', 'От клиента из сайта ФОТОГРАФ');
 //Кому отправить
-$mail->addAdress('lyaisan.333@yandex.ru');
+$mail->addAddress('test-sj8sgsyyx@srv1.mail-tester.com');
 //Тема письма
 $mail->Subject = 'Привет! Это "Фрилансер по жизни"';
 
@@ -25,45 +26,46 @@ $hand = "Левая";
 //Тело письма
 $body = '<h1>Встречай супер письмо!</h1>';
 
-if(trim(!emty($_POST['name']))){
+if(trim(!empty($_POST['name']))){
 $body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
 }
-if(trim(!emty($_POST['email']))){
+if(trim(!empty($_POST['email']))){
 $body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
 }
-if(trim(!emty($_POST['hand']))){
+if(trim(!empty($_POST['hand']))){
 $body.='<p><strong>Рука:</strong> '.$_POST['hand'].'</p>';
 }
-if(trim(!emty($_POST['age']))){
+if(trim(!empty($_POST['age']))){
 $body.='<p><strong>Возраст:</strong> '.$_POST['age'].'</p>';
 }
-if(trim(!emty($_POST['message']))){
+if(trim(!empty($_POST['message']))){
 $body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
 }
 
 //Прикрепить файл
 if (!empty($_FILES['image']['tmp_name'])) {
-//Путь загрузки файла
-$filePath = __DIR__ . "/files/" . $_FILES['image']['name'];
-//грузим файл
-if (copy($_FILES)['image']['tmp_name'], $filePath)){
-$fileAttach = $filePath;
-$body.='<p><strong>Фото в приложении</strong>';
-  $mail->addAttachment($fileAttach);
+  //Путь загрузки файла
+   $filePath = __DIR__ . "/files/" . $_FILES['image']['name'];
+  //грузим файл
+  if (copy($_FILES['image']['tmp_name'], $filePath)){
+    $fileAttach = $filePath;
+    $body.='<p><strong>Фото в приложении</strong></p>';
+      $mail->addAttachment($fileAttach);
   }
-  }
+}
+
 
   $mail->Body = $body;
 
   //Отправляем
   if (!$mail->send()) {
-  $message = 'Ошибка';
+    $message = 'Ошибка';
   } else {
-  $message = 'Данные отправлены!';
-  }
+    $message = 'Данные отправлены!';
+  }  
+    
 
   $response = ['message' => $message];
 
   header('Content-type: application/json');
   echo json_encode($response);
-  ?>
